a1ea53e7f6fb28504e4cfecc58bce11a874f6a5d
Akiko <130021333+Jess90202@users.noreply.github.com>
2026-03-26 21:23:08 -0600

    Add files via upload

---
+++ extension/pkg/ghost_prompt_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_ghostsession_free: (a: number, b: number) => void;
+export const ghostsession_anonymize: (a: number, b: number, c: number) => [number, number];
+export const ghostsession_clear: (a: number) => void;
+export const ghostsession_new: () => number;
+export const ghostsession_rehydrate: (a: number, b: number, c: number) => [number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
